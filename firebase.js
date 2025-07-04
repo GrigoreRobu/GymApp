45b290635ec8e7b1de444fc79d86344e60e6c108
Grigore Robu <grigorerobu.r@gmail.com>
2025-07-04 13:10:13 +0300

    idk

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpsY29dr3gvk5pnXcbohrn8DogDJDteeY",
+  authDomain: "gymapp-985bd.firebaseapp.com",
+  projectId: "gymapp-985bd",
+  storageBucket: "gymapp-985bd.firebasestorage.app",
+  messagingSenderId: "792661325428",
+  appId: "1:792661325428:web:19257fd16e1374271c0321"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
