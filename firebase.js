18e265ff21c26ded8933f3c19bc92d9397700063
Grigore Robu <grigorerobu.r@gmail.com>
2025-07-04 18:00:58 +0300

    changes a lot

--- firebase.js
+++ firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDpsY29dr3gvk5pnXcbohrn8DogDJDteeY",
-  authDomain: "gymapp-985bd.firebaseapp.com",
-  projectId: "gymapp-985bd",
-  storageBucket: "gymapp-985bd.firebasestorage.app",
-  messagingSenderId: "792661325428",
-  appId: "1:792661325428:web:19257fd16e1374271c0321"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -11,12 +11,12 @@ function Galerie() {
     <div className="p-1">
       <h1 className="text-4xl flex font-bold justify-center items-center mb-3">Galerie</h1>
       <div className=' flex gap-4 justify-center flex-wrap'>
-        <img src={img1} className='max-w-[420px] h-auto'></img>
-        <img src={img2} className='max-w-[420px] h-auto'></img>
-        <img src={img3} className='max-w-[420px] h-auto'></img>
-        <img src={img4} className='max-w-[420px] h-auto'></img>
-        <img src={img5} className='max-w-[420px] h-auto'></img>
-        <img src={img6} className='max-w-[420px] h-auto'></img>
+        <img src={img1} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
+        <img src={img2} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
+        <img src={img3} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
+        <img src={img4} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
+        <img src={img5} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
+        <img src={img6} className='max-w-[90%] md:max-w-[420px] h-auto'></img>
       </div>
     </div>
   );
